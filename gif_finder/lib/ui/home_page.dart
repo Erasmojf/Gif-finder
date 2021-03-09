70bec44f9b165ad95d46a9d024cdcc85bd5d9afe
Erasmo J.F Da Silva <erasmogohiper@gmail.com>
2021-03-09 11:19:20 -0300

    CM - ajustes

--- gif_finder/lib/ui/home_page.dart
+++ gif_finder/lib/ui/home_page.dart
@@ -1,4 +1,5 @@
 
+
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -12,16 +13,18 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   String _search;
-  int _offset = 0;
+  http.Response response;
+  
 
- Future _getGifs() async{
-    http.Response response;
-    if(_search == null)
-        response = await  http.get("https://api.giphy.com/v1/gifs/trending?api_key=VFceFvZszLkljPs1UcZak4xzppJBZIHP&limit=20&rating=g");
-    else
-    response = await http.get("https://api.giphy.com/v1/gifs/search?api_key=2h69QPxkAKjn6CwO4XNpvXWxF6TP4CXL&q=$_search&limit=19&offset=$_offset&rating=g&lang=en");
-    return json.decode(response.body);
+ Future<Map> _getGifs() async{
+ if(_search == null){
+    response = await http.get("https://api.giphy.com/v1/gifs/trending?api_key=wAYmtIbZHYugai9263E6hrPVJSgZvXDQ&limit=20&rating=G");
+  }else {
+    response = await http.get("https://api.giphy.com/v1/gifs/search?api_key=wAYmtIbZHYugai9263E6hrPVJSgZvXDQ&q=$_search&limit=25&offset=$_offSet&rating=G&lang=en");
+  } return json.decode(response.body);
   }
+
+
   @override
   Widget build(BuildContext context) {
     return Container(
